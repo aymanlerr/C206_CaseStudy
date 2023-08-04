ebc99fe843780b011ae3178f22deb19c40107a4e
22015131 <22015131@myrp.edu.sg>
2023-08-04 19:37:02 +0800

    Created feature to register for CCA and maintain CCA

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -58,6 +58,12 @@ public class C206_CaseStudy {
 					menu(role);
 				} else if (option == 2) {
 					displayCCA(ccaList);
+				} else if (option == 3) {
+					if (role.equals("student")) {
+						registerCCA(ccaList, userList, applicationList, user_ID);
+					} else {
+						maintainCCA(ccaList);
+					}
 				} else if (option == OPTION_QUIT) {
 					System.out.println("See you again!");
 				} else {
@@ -149,6 +155,275 @@ public class C206_CaseStudy {
 			output += ccaList.get(i).display();
 		}
 		System.out.println(output);
+	}
+	
+	// CHECK IF CCA ID IS CORRECT / EXIST
+	private static boolean checkExistingCcaID(ArrayList<CCA> ccaList, int cca_ID) {
+		for (int i = 0; i < ccaList.size(); i++) {
+			if (cca_ID == ccaList.get(i).getCcaID()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// CHECK FOR EXISTING APPLICATION
+	private static boolean checkExistingApplication(ArrayList<Application> applicationList, int user_ID, int cca_ID) {
+		for (int i = 0; i < applicationList.size(); i++) {
+			if (user_ID == applicationList.get(i).getUser_ID() && cca_ID == applicationList.get(i).getCca_ID()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// GET NUMBER OF APPLIED CCAs
+	private static int getAppliedCCA(ArrayList<Application> applicationList, int user_ID) {
+		int appliedCCAs = 0;
+		for (int i = 0; i < applicationList.size(); i++) {
+			if (user_ID == applicationList.get(i).getUser_ID()) {
+				appliedCCAs++;
+			}
+		}
+		return appliedCCAs;
+	}
+	
+	// REGISTER FOR CCA
+	private static void registerCCA(ArrayList<CCA> ccaList, ArrayList<User> userList,
+			ArrayList<Application> applicationList, int user_ID) {
+		displayCCA(ccaList);
+		int cca_ID = Helper.readInt("Enter CCA ID that you want to join > ");
+
+		// CHECK CCA ID IS CORRECT
+		boolean checkCCAID = checkExistingCcaID(ccaList, cca_ID);
+
+		// CHECK EXISTING APPLICATION
+		boolean checkExistingApplication = checkExistingApplication(applicationList, user_ID, cca_ID);
+
+		// CHECK NUMBER OF APPLICATION
+		int appliedCCAs = getAppliedCCA(applicationList, user_ID);
+
+		// ADD APPLICATION IF ALL CHECKING STAGES PASSED
+		if (checkCCAID == false) {
+			Helper.line(100, "-");
+			System.out.println("Incorrect CCA ID");
+			Helper.line(100, "-");
+		} else if (checkExistingApplication == true) {
+			Helper.line(100, "-");
+			System.out.println("You have already applied for this CCA");
+			Helper.line(100, "-");
+		} else if (appliedCCAs == 2) {
+			Helper.line(100, "-");
+			System.out.println("You have applied for 2 CCas. Maximum application is 2.");
+			Helper.line(100, "-");
+		} else {
+			// GET CCA NAME AND TIMESLOT
+			String cca_name = "";
+			String timeslot = "";
+			for (int i = 0; i < ccaList.size(); i++) {
+				if (cca_ID == ccaList.get(i).getCcaID()) {
+					cca_name = ccaList.get(i).getCca_name();
+					timeslot = ccaList.get(i).getTimeslot();
+				}
+			}
+
+			// ADD INTO APPLICATION LIST
+			for (int i = 0; i < userList.size(); i++) {
+				if (user_ID == userList.get(i).getUserID()) {
+					applicationList.add(new Application(cca_ID, cca_name, timeslot, userList.get(i).getUserID(),
+							userList.get(i).getName(), ""));
+					Helper.line(100, "-");
+					System.out.println("You have succesfully registered for this cca");
+					Helper.line(100, "-");
+				}
+			}
+		}
+	}
+	
+	// CHECK IF TIMESLOT EXIST WITH SAME CCA NAME
+	private static boolean checkExistingTimeslotAndName(ArrayList<CCA> ccaList, String timeslot, String cca_name) {
+		for (int i = 0; i < ccaList.size(); i++) {
+			if (timeslot.equalsIgnoreCase(ccaList.get(i).getTimeslot())
+					&& cca_name.equalsIgnoreCase(ccaList.get(i).getCca_name())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// CHECK IF CCA NAME IS CORRECT
+	private static boolean checkExistingCcaName(ArrayList<CCA> ccaList, String cca_name) {
+		for (int i = 0; i < ccaList.size(); i++) {
+			if (cca_name.equals(ccaList.get(i).getCca_name())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
+	// MAINTAIN CCA
+	private static void maintainCCA(ArrayList<CCA> ccaList) {
+		displayCCA(ccaList);
+		Helper.line(100, "-");
+		System.out.println("1. Add CCA\n2. Delete CCA\n3. Edit CCA\n4. Back");
+		Helper.line(100, "-");
+		int option = Helper.readInt("\nEnter your option > ");
+
+		if (option == 1) {
+
+			int cca_ID = 1;
+			boolean getId = false;
+			ArrayList<Integer> ccaIds = new ArrayList<>();
+			for (int i = 0; i < ccaList.size(); i++) {
+				ccaIds.add(ccaList.get(i).getCcaID());
+			}
+
+			while (getId == false) {
+				if (ccaIds.contains(cca_ID)) {
+					cca_ID++;
+				} else {
+					getId = true;
+				}
+			}
+
+			String cca_name = Helper.readString("Enter new CCA name > ");
+			String desc = Helper.readString("Enter new CCA description > ");
+			String timeslot = Helper
+					.readString("Enter new timeslot    Format=> day[spelled out]: [#][pm/am]-[#][pm/am] > ");
+
+			// CHECK IF NEW CCA ID IS VALID
+			boolean checkExistingCcaID = checkExistingCcaID(ccaList, cca_ID);
+
+			// CHECK IF NEW TIMESLOT IS VALID
+			boolean checkExistingTimeslot = checkExistingTimeslotAndName(ccaList, timeslot, cca_name);
+
+			// ADD IF ALL CHECKS PASSED
+			if (checkExistingCcaID == true) {
+				Helper.line(100, "-");
+				System.out.println("CCA ID already exist. Please use another ID.");
+				Helper.line(100, "-");
+			} else if (checkExistingTimeslot == true) {
+				Helper.line(100, "-");
+				System.out.println("Timeslot for the CCA already exist. Please enter another timeslot");
+				Helper.line(100, "-");
+			} else {
+				ccaList.add(new CCA(cca_ID, cca_name, desc, timeslot));
+				Helper.line(100, "-");
+				System.out.println("CCA successfully added");
+				Helper.line(100, "-");
+			}
+		} else if (option == 2) {
+			int cca_ID = Helper.readInt("Enter CCA ID to delete > ");
+			String cca_name = Helper.readString("Enter CCA name to confirm > ");
+
+			// CHECK IF CCA ID EXIST
+			boolean checkExistingCcaID = checkExistingCcaID(ccaList, cca_ID);
+
+			// CHECK IF CCA NAME IS CORRECT
+			boolean checkExistingCcaName = checkExistingCcaName(ccaList, cca_name);
+
+			// DELETE CCA IF ALL CHECKS PASSED
+			if (checkExistingCcaID == false) {
+				Helper.line(100, "-");
+				System.out.println("Invalid CCA ID");
+				Helper.line(100, "-");
+			} else if (checkExistingCcaName == false) {
+				Helper.line(100, "-");
+				System.out.println("Invalid CCA name");
+				Helper.line(100, "-");
+			} else {
+				for (int i = 0; i < ccaList.size(); i++) {
+					if (cca_ID == ccaList.get(i).getCcaID() && cca_name.equals(ccaList.get(i).getCca_name())) {
+						ccaList.remove(i);
+						Helper.line(100, "-");
+						System.out.println("CCA successfully removed");
+						Helper.line(100, "-");
+					}
+				}
+			}
+
+		} else if (option == 3) {
+			int cca_ID = Helper.readInt("Enter CCA ID to edit > ");
+
+			// CHECK IF CCA ID EXIST
+			boolean checkExistingCcaID = checkExistingCcaID(ccaList, cca_ID);
+
+			// GET CCA POSITION
+			int position = 0;
+			if (checkExistingCcaID == false) {
+				Helper.line(100, "-");
+				System.out.println("Invalid CCA ID");
+				Helper.line(100, "-");
+			} else {
+				for (int i = 0; i < ccaList.size(); i++) {
+					if (cca_ID == ccaList.get(i).getCcaID()) {
+						position = i;
+					}
+				}
+			}
+
+			// EDIT
+			System.out.println("1. CCA Name\n2. CCA Description\n3. Timeslot\n4. Back");
+			int choice = Helper.readInt("\nEnter option to edit > ");
+			// CCA NAME
+			if (choice == 1) {
+				String newCcaName = Helper.readString("Enter new CCA Name > ");
+
+				// CHECK TIMESLOT
+				boolean checkExistingTimeslot = checkExistingTimeslotAndName(ccaList,
+						ccaList.get(position).getTimeslot(), newCcaName);
+
+				// UPDATE IF CHECKS PASSED
+				if (checkExistingTimeslot == true) {
+					Helper.line(100, "-");
+					System.out.println("CCA name with same timeslot already exist");
+					Helper.line(100, "-");
+				} else {
+					ccaList.get(position).setCca_name(newCcaName);
+					Helper.line(100, "-");
+					System.out.println("CCA name successfully updated");
+					Helper.line(100, "-");
+				}
+
+				// CCA DESC
+			} else if (choice == 2) {
+				String newDesc = Helper.readString("Enter new CCA description > ");
+				ccaList.get(position).setDesc(newDesc);
+
+				// CCA TIMESLOT
+			} else if (choice == 3) {
+				String newCcaTimeslot = Helper
+						.readString("Enter new timeslot    Format=> day[spelled out]: [#][pm/am]-[#][pm/am] > ");
+
+				// CHECK TIMESLOT
+				boolean checkExistingTimeslot = checkExistingTimeslotAndName(ccaList, newCcaTimeslot,
+						ccaList.get(position).getCca_name());
+
+				// UPDATE IF CHECKS PASSED
+				if (checkExistingTimeslot == true) {
+					Helper.line(100, "-");
+					System.out.println("CCA name with same timeslot already exist");
+					Helper.line(100, "-");
+				} else {
+					ccaList.get(position).setTimeslot(newCcaTimeslot);
+					Helper.line(100, "-");
+					System.out.println("CCA name successfully updated");
+					Helper.line(100, "-");
+				}
+
+				// BACK
+			} else if (choice == 4) {
+
+			}
+
+		} else if (option == 4) {
+
+		} else {
+			Helper.line(100, "-");
+			System.out.println("Invalid option");
+			Helper.line(100, "-");
+		}
 	}
 
 }
