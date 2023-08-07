862b9312beb1beb2582af874b6a35c8d8a0baedb
chavonne-kek <22017979@myrp.edu.sg>
2023-08-07 10:24:32 +0800

    testAddUser()

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -19,10 +19,10 @@ public class C206_CaseStudyTest {
 	
 	private Application app1;
 	
-	private ArrayList<User> userList;
-	private ArrayList<CCA> ccaList;
-	private ArrayList<Application> applicationList;
-	private ArrayList<Attendance> attendanceList;
+	private ArrayList<User> userList = new ArrayList<>();
+	private ArrayList<CCA> ccaList = new ArrayList<>();
+	private ArrayList<Application> applicationList = new ArrayList<>();
+	private ArrayList<Attendance> attendanceList = new ArrayList<>();
 
 	
 	public C206_CaseStudyTest() {
@@ -31,11 +31,11 @@ public class C206_CaseStudyTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		user1 = (new User(1, "Aiman", "1", "student"));
-		user2 = (new User(2, "Aldusto", "2", "teacher"));
-		user3 = (new User(3, "kedongdong", "3", "admin"));
-		user4 = (new User(4, "Liam", "4", "student"));
-		user5 = (new User(5, "Amran", "5", "student"));
+		user1 = new User(1, "Aiman", "1", "student");
+		user2 = new User(2, "Aldusto", "2", "teacher");
+		user3 = new User(3, "kedongdong", "3", "admin");
+		user4 = new User(4, "Liam", "4", "student");
+		user5 = new User(5, "Amran", "5", "student");
 		
 	
 		cca1 = (new CCA(1, "Soccer", "Have fun playing soccer in this CCA!", "Wednesday: 4pm-6pm"));
@@ -54,19 +54,17 @@ public class C206_CaseStudyTest {
 	}
 	
 	@Test
-	public void testMaintainUsers() {
-		C206_CaseStudy.maintainUsers(userList,ccaList);
-		// Add user
-		userList.add(user1);
-		assertEquals("Test that infomation are submitted correctly", user1, userList.get(0));
-		// Edit user
-		String newName = "John";
-		userList.get(0).setName(newName);
-		assertEquals("Test that editing of user info will be shown correctly", userList.get(0).getName(), newName);
-		// Delete user
-		userList.remove(user1);
-		assertEquals("Test that deletion user will be shown correctly", 0, userList.size());
+	public void testAddUsers() {
+		int userId = 1;
+		String username = "Aiman";
+		String password = "1";
+		String role = "student";
 		
+		assertNotNull("Test if there is valid user arrayList to add to", userList);
+		assertEquals("Test that the user arrayList is empty", 0, userList.size());
+		// Test Add user
+		C206_CaseStudy.addUser(userList, userId, username, password, role);
+		assertEquals("Test that arrayList increases size to 1", user1, userList.size());
 	}
 	
 	@Test
