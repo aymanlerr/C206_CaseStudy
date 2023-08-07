40429ce62391d79781316f00cb35ee3ee7fd615e
jaymi <22036150@myrp.edu.sg>
2023-08-07 10:33:55 +0800

    Added test code for adding approval status to application, adjusted text of other test code

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -19,10 +19,10 @@ public class C206_CaseStudyTest {
 	
 	private Application app1;
 	
-	private ArrayList<User> userList = new ArrayList<>();
-	private ArrayList<CCA> ccaList = new ArrayList<>();
-	private ArrayList<Application> applicationList = new ArrayList<>();
-	private ArrayList<Attendance> attendanceList = new ArrayList<>();
+	private ArrayList<User> userList;
+	private ArrayList<CCA> ccaList;
+	private ArrayList<Application> applicationList;
+	private ArrayList<Attendance> attendanceList;
 
 	
 	public C206_CaseStudyTest() {
@@ -31,18 +31,18 @@ public class C206_CaseStudyTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		user1 = new User(1, "Aiman", "1", "student");
-		user2 = new User(2, "Aldusto", "2", "teacher");
-		user3 = new User(3, "kedongdong", "3", "admin");
-		user4 = new User(4, "Liam", "4", "student");
-		user5 = new User(5, "Amran", "5", "student");
+		user1 = (new User(1, "Aiman", "1", "student"));
+		user2 = (new User(2, "Aldusto", "2", "teacher"));
+		user3 = (new User(3, "kedongdong", "3", "admin"));
+		user4 = (new User(4, "Liam", "4", "student"));
+		user5 = (new User(5, "Amran", "5", "student"));
 		
 	
 		cca1 = (new CCA(1, "Soccer", "Have fun playing soccer in this CCA!", "Wednesday: 4pm-6pm"));
 		cca2 = (new CCA(2, "Badminton", "Have fun playing badminton in this CCA!", "Wednesday: 4pm-6pm"));
 		cca3 = (new CCA(3, "Tennis", "Have fun playing tennis in this CCA!", "Friday: 4pm-6pm"));
 		
-		app1 = (new Application(1, "Soccer", "Wednesday: 4pm-6pm", 1, "Aiman", "approved"));
+		app1 = (new Application(1, "Soccer", "Wednesday: 4pm-6pm", 1, "gh", "gh"));
 		
 	}
 
@@ -54,25 +54,29 @@ public class C206_CaseStudyTest {
 	}
 	
 	@Test
-	public void testAddUsers() {
-		int userId = 1;
-		String username = "Aiman";
-		String password = "1";
-		String role = "student";
+	public void testMaintainUsers() {
+		C206_CaseStudy.maintainUsers(userList,ccaList);
+		// Add user
+		userList.add(user1);
+		assertEquals("Test that infomation are submitted correctly", user1, userList.get(0));
+		// Edit user
+		String newName = "John";
+		userList.get(0).setName(newName);
+		assertEquals("Test that editing of user info will be shown correctly", userList.get(0).getName(), newName);
+		// Delete user
+		userList.remove(user1);
+		assertEquals("Test that deletion user will be shown correctly", 0, userList.size());
 		
-		assertNotNull("Test if there is valid user arrayList to add to", userList);
-		assertEquals("Test that the user arrayList is empty", 0, userList.size());
-		// Test Add user
-		C206_CaseStudy.addUser(userList, userId, username, password, role);
-		assertEquals("Test that arrayList increases size to 1", user1, userList.size());
 	}
 	
 	@Test
-	public void testDeleteApproval() {
-	
-		//test for existing approval arraylist
+	public void testDeleteApplication() {
+		//new application arraylist
+		applicationList = new ArrayList<>();
+		
+		//test for existing application arraylist
 		//test for empty arraylist
-		assertNotNull("Test if there is any approval arraylist to delete from", applicationList);
+		assertNotNull("Test if there is any application arraylist to delete from", applicationList);
 		assertEquals("Test that arraylist is empty", 0, applicationList.size());
 		
 		//add item in arraylist
@@ -85,13 +89,39 @@ public class C206_CaseStudyTest {
 		//remove item in arraylist
 		//test that applicationList is 0
 		//test that item is removed from arraylist
-		C206_CaseStudy.deleteApplication(applicationList, 1, 1);
+		applicationList.remove(app1);
 		assertEquals("Test that arraylist size is 0", 0, applicationList.size());
 		
+		C206_CaseStudy.deleteApplication(applicationList, 1, 1);
 	}
-
+	
+	@Test
+	public void testAddApproval() {
+		//new application arraylist
+		applicationList = new ArrayList<>();
+		
+		//test for existing application arraylist
+		//test for empty arraylist
+		assertNotNull("Test if there is any application arraylist to delete from", applicationList);
+		assertEquals("Test that arraylist is empty", 0, applicationList.size());
+		
+		//add application to maintain
+		applicationList.add(app1);
+		
+		//test that applicationList is 1 (contains application)
+		//test that changes made to application is reflected
+		String newStatus = "Approval";
+		applicationList.get(0).setStatus(newStatus);
+		assertEquals("Test that changes that are made to an application’s status is reflected in the database accurately", applicationList.get(0).getStatus(), newStatus);
+		
+		
+		
+	}
+	
+	
 @After
 	public void tearDown() throws Exception {
 	}
+
 
 }
